228564b78f8567e8aeb64b25cbd3a933b2f495d0
Rishabh <rishabhmaheshwari1999@gmail.com>
2021-03-06 19:53:39 +0530

    What is function

---
+++ Lec0/functions.js
@@ -0,0 +1,31 @@
+// function body // function statement
+function sayHi(){
+    console.log("Hello Func");
+}
+//functions are variables in js
+// function call 
+// sayHi();
+// console.log(sayHi());
+
+// or
+// let value = sayHi();
+// console.log(value);
+
+console.log(sayHi);
+
+function sayHi(name){
+    console.log( name + " says Hiii !!!");
+    return 10;
+}
+
+// functions are variables 
+
+// function call
+// sayHi();
+
+// let value = sayHi();
+// console.log( value );
+// console.log(sayHi);
+
+let value = sayHi("Steve");
+console.log(value);
