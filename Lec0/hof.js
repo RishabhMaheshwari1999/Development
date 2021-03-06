a45eb4fddf85722e63cf6e01c0a60d02d9c5e9a6
Rishabh <rishabhmaheshwari1999@gmail.com>
2021-03-06 19:56:20 +0530

    High order Functions

---
+++ Lec0/hof.js
@@ -0,0 +1,23 @@
+// High Order Functions => Functions which accept functions as a argument !
+// Callback Functions => Functions which are passed into a function as a argument !
+
+function getFirstName(fullName){
+    // fullName = "TONY STARK".split(" ") => [ "TONY" , "STARK"  ];// here split is used to split the string from the " ".
+    fullName = fullName.split(" ");
+    return fullName[0];
+}
+
+function getLastName(fullName){
+    fullName = fullName.split(" ");
+    return fullName[1];
+}
+
+function sayHi( fullName  , fun  ){
+    let name = fun(fullName);
+    console.log(name);
+}
+
+
+sayHi("TONY STARK" , getFirstName);
+sayHi("BRUCE BANNER" ,  getLastName);
+//so the output will be tony banner
