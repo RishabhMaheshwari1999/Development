27dc1f3cc81d71431a22185c8f26444777369f43
Rishabh <rishabhmaheshwari1999@gmail.com>
2021-06-12 20:24:14 +0530

    Working with React

---
+++ Module3_React/React_Demo/script.js
@@ -0,0 +1,5 @@
+//https://reactjs.org/docs/add-react-to-a-website.html    to add react
+// what to render , and where to render
+ReactDOM.render(
+    React.createElement("h1", null, "Hey i am rendered from react !!"), document.querySelector("#root")
+);
