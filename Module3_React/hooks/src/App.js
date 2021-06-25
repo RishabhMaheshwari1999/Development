431911ed10a5e9c837af2ad879ddc84c7f4a83ec
Rishabh <rishabhmaheshwari1999@gmail.com>
2021-06-25 21:22:43 +0530

    UseEffect

--- Module3_React/hooks/src/App.js
+++ Module3_React/hooks/src/App.js
@@ -1,9 +1,10 @@
 import UseState from "./Components/UseState";
-
+import UseEffect from "./Components/UseEffect";
 function App() {
   return (
     <div className="App">
-      <UseState></UseState>
+      {/* <UseState></UseState> */}
+      <UseEffect></UseEffect>
     </div>
   );
 }
